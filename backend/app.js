42775254b74dafa85421a7e9f17d50390e46df39
Yousef <ioyx515@gmail.com>
2025-01-17 21:23:27 +0200

    updated app.js with compression middleware and added comments

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
@@ -6,15 +6,28 @@ const AppError = require('./utils/appError');
 const tasksRouter = require('./routes/tasks.routes');
 const usersRouter = require('./routes/users.routes');
 const rateLimiter = require('./middlewares/rateLimit');
+const compression = require('compression');
 
-
+// Enable CORS
 app.use(cors());
-app.use(morgan('dev'));
+
+// Use compression for response body compression
+app.use(compression());
+
+// Enable logging in development
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Body parser middleware
 app.use(express.json());
+
+// Rate limiter middleware
 app.use(rateLimiter);
+
+// Routes
 app.use('/api/v1/tasks', tasksRouter);
 app.use('/api/v1/users', usersRouter);
-
 
 // Global Middleware for not found routes
 app.all('*', (req, res, next) => {
