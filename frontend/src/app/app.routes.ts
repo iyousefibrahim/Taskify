c30e21465d5ef0072ae69da18f121836aec58a68
Yousef <ioyx515@gmail.com>
2025-01-17 23:57:10 +0200

    feat: implement authentication components, routing, and not-found component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", component: AuthLayoutComponent, children: [
+            { path: "", redirectTo: "login", pathMatch: 'full' },
+            { path: "login", component: LoginComponent, title: "Login" },
+            { path: "register", component: RegisterComponent, title: "Register" },
+        ]
+    },
+    { path: "**", component: NotfoundComponent, title: "404" }
+];
